316a1f6ddaae73f327ee7618118b03594bc61feb
Sun-Kyoung <skc4365@gmail.com>
2021-11-01 10:32:41 +0900

    퀵정렬 수정

--- classExSortQuick.py
+++ classExSortQuick.py
@@ -1,70 +1,39 @@
 import numpy as np
 
-#===== 퀵 정렬 =====
-# arrNum = np.array([57, 33, 41, 91, 7, 18, 52, 23], int)
-# 예제는 홀수개를 준비하여, 정가운데 4를 pivot이라 가정하겠다.
-arrNum = np.array([7, 5, 1, 4, 6, 3, 2], int)
+#arrNum = np.array([7, 5, 1, 4, 6, 3, 2], int)
+arrNum = np.array([5, 3, 8, 4, 9, 1, 6, 2, 7], int)
 
-# 출력을 위한 변수 ----- START
-arrResult = np.array([], int)
-firstPivot = len(arrNum) // 2
-cnt = 0
-# -------------------- END
-
-def sortQuick(arr):
-    global arrResult
+def partition(arr, left, right):
+    low = left +1
+    high = right
+    pivot = arr[left]
+    while (low <= high):
+        while low <= right and arr[low] < pivot:
+            low += 1
+        while high >= left and arr[high] > pivot:
+            high -= 1
+            
+        if low < high:
+            arr[low], arr[high] = arr[high], arr[low]
+            print("교환: {}" .format(arrNum))
+            
+    arr[left], arr[high] = arr[high], arr[left]
+    print(arrNum)
     
-    # 
-    if len(arr) <= 1:
-        return arr
+    print("pivot_________________[{}]" .format(high))
+    return high
     
-    # 빈배열 정의_[작은값] [중앙값][큰값]
-    arrLess = np.array([], int)
-    arrEqual = np.array([], int)
-    arrGreat = np.array([], int)
-    
-    # 연산자// : 나누기한 값을 정수로 가져오기(소숫점버림)
-    pivot = arr[len(arr) // 2]
-    
-    for n in arr:
-        if n < pivot:
-            arrLess = np.append(arrLess, n)
-            # print("arrLess: {}" .format(n))
-        elif n > pivot:
-            arrGreat = np.append(arrGreat, n)
-            # print("arrGreat: {}" .format(n))
-        else:
-            arrEqual = np.append(arrEqual, n)
-            # print("arrEqual: {}" .format(n))
-
-    print("-----")
-    print("리턴값 {} - {} - {}" .format(arrLess, arrEqual, arrGreat))
-    
-    global cnt
-    if cnt < firstPivot:
-        arrResult = np.insert(arrResult, 0, arrEqual)
-        cnt += 1    
-    else:
-        arrResult = np.append(arrResult, pivot)
-    
-    if len(arrLess) == 1 :
-        arrResult = np.insert(arrResult, 0, arrLess)
-        print(arrEqual)
-        print(arrLess)   
-    elif len(arrGreat) == 1 :
-        arrResult = np.append(arrResult, arrGreat)
-        print(arrEqual)
-        print(arrGreat)
-    else :
-        print(arrEqual)
-    
-    return sortQuick(arrLess) + arrEqual + sortQuick(arrGreat)
-    
-    print(pivot)
+def sortQuick(arr, left, right):
+    if left < right :
+        p = partition(arr, left, right)
+        sortQuick(arr, left, p - 1)
+        sortQuick(arr, p + 1, right)
+        
 
 #_____ 출력
 print("기본데이터: {}" .format(arrNum))
 print("===============================")
-sortQuick(arrNum)
+print("pivot_________________[0]")
+sortQuick(arrNum, 0, len(arrNum)-1)
 print("===============================")
-print("퀵정렬 수행결과\n{}" .format(arrResult))
+print("퀵정렬 수행결과\n{}" .format(arrNum))
