1392a837259e0e4192ddf8c398ffb924777f3147
Sun-Kyoung <skc4365@gmail.com>
2021-11-04 11:55:15 +0900

    쿽정렬 2차수정

--- classExSortQuick.py
+++ classExSortQuick.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 #arrNum = np.array([7, 5, 1, 4, 6, 3, 2], int)
-arrNum = np.array([5, 3, 8, 4, 9, 1, 6, 2, 7], int)
+#arrNum = np.array([5, 3, 8, 4, 9, 1, 6, 2, 7], int)
+arrNum = np.array([54, 34, 41, 89, 67, 16, 52, 23], int)
 
 def partition(arr, left, right):
     low = left +1
     high = right
     pivot = arr[left]
+    print("pivot값===================: {}" .format(arr[left]))
+    
+    print("_low: {}, high: {}" .format(arr[low], arr[high]))
     while (low <= high):
         while low <= right and arr[low] < pivot:
             low += 1
@@ -18,9 +22,9 @@ def partition(arr, left, right):
             print("교환: {}" .format(arrNum))
             
     arr[left], arr[high] = arr[high], arr[left]
-    print(arrNum)
+    print("교환결과_____: ", arrNum)
     
-    print("pivot_________________[{}]" .format(high))
+    print("high값______________: {}" .format(arr[high]))
     return high
     
 def sortQuick(arr, left, right):
@@ -33,7 +37,7 @@ def sortQuick(arr, left, right):
 #_____ 출력
 print("기본데이터: {}" .format(arrNum))
 print("===============================")
-print("pivot_________________[0]")
+#print("pivot값_________________: {}" .format(arrNum[0]))
 sortQuick(arrNum, 0, len(arrNum)-1)
 print("===============================")
 print("퀵정렬 수행결과\n{}" .format(arrNum))
